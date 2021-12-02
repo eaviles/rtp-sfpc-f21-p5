39edfb4c689def7a9079ad58d72ae1b7ea7a80d6
Edgardo Avilés-López <eaviles@gmail.com>
2021-12-02 15:14:37 -0800

    Add "image-average" to week 6

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rtp-sfpc-f21-p5
+
 RTP class at SFPC, Fall 2021 (p5 version)
+
+This repository is a p5.js version of the repository by Zach Lieberman: https://github.com/ofZach/RTP_SFPC_F21/
+
+Use the files named `sketch.js` in each folder as a reference.
+
+To run a local server with these sketches run the next commands after cloning the repository:
+
+1. `npm install`
+2. `npm start`
---
+++ week6/image-average/sketch.js
@@ -0,0 +1,41 @@
+const NUM_OF_IMAGES = 4;
+const WIDTH = 640;
+const HEIGHT = 480;
+
+const images = [];
+
+function preload() {
+  for (let i = 0; i < NUM_OF_IMAGES; i += 1) {
+    const fileName = `images/polar-bear-${i}.jpg`;
+    images.push(loadImage(fileName));
+  }
+}
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
+}
+
+function draw() {
+  let average = createImage(WIDTH, HEIGHT);
+  average.loadPixels();
+  for (let i = 0; i < WIDTH; i += 1) {
+    for (let j = 0; j < HEIGHT; j += 1) {
+      let sumRed = 0;
+      let sumGreen = 0;
+      let sumBlue = 0;
+      for (let k = 0; k < NUM_OF_IMAGES; k += 1) {
+        const color = images[k].get(i, j);
+        sumRed += color[0];
+        sumGreen += color[1];
+        sumBlue += color[2];
+      }
+      sumRed /= NUM_OF_IMAGES;
+      sumGreen /= NUM_OF_IMAGES;
+      sumBlue /= NUM_OF_IMAGES;
+      average.set(i, j, color(sumRed, sumGreen, sumBlue));
+    }
+  }
+  average.updatePixels();
+  image(average, 0, 0);
+  noLoop();
+}
