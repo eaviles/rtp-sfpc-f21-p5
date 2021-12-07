a082264306191e5c184ad3339771efbac52be596
Edgardo Avilés-López <eaviles@gmail.com>
2021-12-07 18:28:44 -0500

    Add "image-3-example" to week 5

---
+++ week-5/image-3-example/sketch.js
@@ -0,0 +1,38 @@
+let srcImg;
+let tmpImg;
+let w, h;
+
+function preload() {
+  srcImg = loadImage("/assets/images/lillian.png");
+}
+
+function resetImg() {
+  tmpImg.copy(srcImg, 0, 0, w, h, 0, 0, w, h);
+  tmpImg.filter(THRESHOLD);
+}
+
+function setup() {
+  pixelDensity(1);
+  w = srcImg.width;
+  h = srcImg.height;
+  createCanvas(w, h * 2);
+  tmpImg = createImage(w, h);
+  resetImg();
+}
+
+function draw() {
+  background(0);
+  image(srcImg, 0, 0);
+  image(tmpImg, 0, h);
+  if (frameCount % 4 === 0) {
+    tmpImg.filter(DILATE);
+  }
+}
+
+function keyPressed() {
+  resetImg();
+}
+
+function mouseClicked() {
+  resetImg();
+}
