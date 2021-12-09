1d38e5cd99a7941c1fd45d86b1147a855c3b1776
Edgardo Avilés-López <eaviles@gmail.com>
2021-12-09 17:58:41 -0500

    Add "rutt-etra-2" to week 7

--- week-7/rutt-etra-2/sketch.js
+++ week-7/rutt-etra-2/sketch.js
@@ -4,31 +4,36 @@ const HEIGHT = 480;
 let capture;
 
 function setup() {
-  createCanvas(WIDTH, HEIGHT);
+  createCanvas(WIDTH, HEIGHT, WEBGL);
   capture = createCapture(VIDEO);
   capture.size(WIDTH, HEIGHT);
   capture.hide();
   pixelDensity(1);
   noFill();
   stroke(255);
+  strokeWeight(2);
+  ortho();
 }
 
 function draw() {
   background(0);
+  orbitControl();
+  scale(0.8);
   capture.loadPixels();
-  for (let y = 0; y < height; y += 5) {
+  for (let y = 0; y < height; y += 15) {
     beginShape();
-    for (let x = 0; x <= width; x += 10) {
+    for (let x = 0; x <= width; x += 20) {
       const off = (y * width + x) * 4;
       const { pixels: pixs } = capture;
       const r = pixs[off];
       const g = pixs[off + 1];
       const b = pixs[off + 2];
       const l = brightness(color(r, g, b));
-      const d = map(l, 0, 255, 0, mouseX, true);
-      curveVertex(x, y + d);
+      const z = map(l, 0, 255, -mouseX, mouseX);
+      const nx = x - width / 2;
+      const ny = y - height / 2;
+      curveVertex(nx, ny, z);
     }
     endShape();
   }
-  image(capture, 0, height * 0.75, width * 0.25, height * 0.25);
 }
